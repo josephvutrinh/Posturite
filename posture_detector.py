04798c394db83f8f209c7fd41302cdf1dd8bbece
Alton Lin <alton.dlin@gmail.com>
2025-03-30 09:41:58 -0400

    shoulder detection

--- posture_detector.py
+++ posture_detector.py
@@ -42,22 +42,34 @@ class PostureDetector:
                 #print(id, lm)
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 cz = int(lm.z)
-                lmList.append([id, cz])
+                lmList.append([id, cy, cz,])
                 if draw:
                    cv2.circle(img, (cx, cy), 10, (255, 0, 0), cv2.FILLED)
         return lmList
 
     def detectForwardHead(self, lmList, threshold = 0.1):
         if lmList:
-            nose_z = lmList[0][1]
-            left_shoulder_z = lmList[11][1]
-            right_shoulder_z = lmList[12][1]
+            nose_z = lmList[0][2]
+            left_shoulder_z = lmList[11][2]
+            right_shoulder_z = lmList[12][2]
 
             shoulder_z_avg = (left_shoulder_z + right_shoulder_z) / 2
 
             if nose_z < shoulder_z_avg - threshold * abs(shoulder_z_avg):
                 return True
         return False
+
+    def detectedShruggedShoulder(self, lmList, threshold = 0.287):
+        if lmList:
+            left_mouth_y = lmList[9][1]
+            right_mouth_y = lmList[10][1]
+            left_shoulder_y = lmList[11][1]
+            right_shoulder_y = lmList[12][1]
+
+            if left_mouth_y < left_shoulder_y - (threshold * 1.2) * abs(
+                left_shoulder_y) and right_mouth_y < right_shoulder_y - (threshold * 1.2) * abs(right_shoulder_y):                return True
+        return False
+
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -69,11 +81,23 @@ def main():
         lmList = detector.findPosition(img)
         print(lmList)
 
-        if detector.detectForwardHead(lmList):
+        if detector.detectForwardHead(lmList) and detector.detectedShruggedShoulder(lmList):
+            cv2.putText(img, "Forward Head and Shrugged Shoulders Detected!", (50, 50),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            print("⚠️Forward Head and Shrugged Shoulders Detected!")
+
+        elif detector.detectedShruggedShoulder(lmList):
+            cv2.putText(img, "Shrugged Shoulder Detected!", (50, 50),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            print("⚠️Shrugged Shoulder Detected!")
+
+        elif detector.detectForwardHead(lmList):
             cv2.putText(img, "Forward Head Detected!", (50, 50),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
             print("⚠️Forward Head Detected!")
-        else:
+
+
+        if not detector.detectForwardHead(lmList) and not detector.detectedShruggedShoulder(lmList) :
             cv2.putText(img, "Good Posture", (50, 50),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
             print("Good")
